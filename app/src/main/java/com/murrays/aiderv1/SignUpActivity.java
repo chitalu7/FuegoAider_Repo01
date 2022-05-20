8f8268239927fc82c345e5252613b49bf79d2716
Eric Sergio <ericdsergio87@icloud.com>
2022-05-20 07:08:34 -0700

    cleaned up, condensed, added one of the needed fields ++

--- app/src/main/java/com/murrays/aiderv1/SignUpActivity.java
+++ app/src/main/java/com/murrays/aiderv1/SignUpActivity.java
@@ -2,17 +2,15 @@ package com.murrays.aiderv1;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+//import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-
-import androidx.annotation.NonNull;
+//import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
+//import com.google.android.gms.tasks.OnCompleteListener;
+//import com.google.android.gms.tasks.Task;
+//import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
@@ -26,7 +24,17 @@ public class SignUpActivity extends AppCompatActivity {
     private FirebaseAuth mFirebaseAuth;
     protected EditText firstName;
     protected EditText lastName;
+    protected EditText phoneEditText;
     private DatabaseReference mDatabase;
+    /*
+    To Do: Update the layout to include relevant data that we need.
+    I used for loops here that will correspond to the text fields. The arrays
+    "UserData" and "userDataValues" should be the same length and should include
+    1) the field name in UserData and 2) the initialized value in userDataValues
+    This way, when we update the layout to contain the fields that we need, when a
+    new user signs up, their information will be stored in the nosql database.
+    */
+    String[] UserData = {"FirstName", "LastName", "Phone", "Email", "Password"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,74 +43,60 @@ public class SignUpActivity extends AppCompatActivity {
 
         // Initialize FirebaseAuth
         mFirebaseAuth = FirebaseAuth.getInstance();
-        firstName = (EditText)findViewById(R.id.firstName);
-        lastName = (EditText)findViewById(R.id.lastName);
+        firstName = findViewById(R.id.firstName);
+        lastName = findViewById(R.id.lastName);
+        phoneEditText = findViewById(R.id.phoneField);
 
-        passwordEditText = (EditText)findViewById(R.id.passwordField);
-        emailEditText = (EditText)findViewById(R.id.emailField);
-        signUpButton = (Button)findViewById(R.id.signupButton);
+        passwordEditText = findViewById(R.id.passwordField);
+        emailEditText = findViewById(R.id.emailField);
+        signUpButton = findViewById(R.id.signupButton);
 
-        signUpButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String password = passwordEditText.getText().toString();
-                String email = emailEditText.getText().toString();
-                String fname = firstName.getText().toString();
-                String lname = lastName.getText().toString();
-                mDatabase = FirebaseDatabase.getInstance().getReference();
+        signUpButton.setOnClickListener(v -> {
+            String password = passwordEditText.getText().toString();
+            String email = emailEditText.getText().toString();
+            String fname = firstName.getText().toString();
+            String lname = lastName.getText().toString();
+            String uphone = phoneEditText.getText().toString();
+            mDatabase = FirebaseDatabase.getInstance().getReference();
 
-                password = password.trim();
-                email = email.trim();
+            password = password.trim();
+            email = email.trim();
 
-                if (fname.isEmpty() || lname.isEmpty()|| password.isEmpty() || email.isEmpty()) {
-                    AlertDialog.Builder builder = new AlertDialog.Builder(SignUpActivity.this);
-                    builder.setMessage(R.string.signup_error_message)
-                            .setTitle(R.string.signup_error_title)
-                            .setPositiveButton(android.R.string.ok, null);
-                    AlertDialog dialog = builder.create();
-                    dialog.show();
-                } else {
-                    mFirebaseAuth.createUserWithEmailAndPassword(email, password)
-                            .addOnCompleteListener(SignUpActivity.this, new OnCompleteListener<AuthResult>() {
-                                @Override
-                                public void onComplete(@NonNull Task<AuthResult> task) {
-                                    if (task.isSuccessful()) {
+           /* if (fname.isEmpty() || lname.isEmpty()|| password.isEmpty() || email.isEmpty()) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(SignUpActivity.this);
+                builder.setMessage(R.string.signup_error_message)
+                        .setTitle(R.string.signup_error_title)
+                        .setPositiveButton(android.R.string.ok, null);
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            } else {*/
+                mFirebaseAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(SignUpActivity.this, task -> {
+                            if (task.isSuccessful()) {
 
-                                        FirebaseUser user = task.getResult().getUser();
-                                        String mUserId = user.getUid();
-                                        mDatabase.child("users").child(mUserId).child("profile").child("FirstName").setValue(fname);
-                                       mDatabase.child("users").child(mUserId).child("profile").child("LastName").setValue(lname);
-                                       // User newuser = new User(fname,lname);
-                                   //     User newuser = new User("Steph","Murray");
-                                  //      Log.d("Test","hi");
-                                    //    Log.d("Hello", newuser.FirstName);
-                                      //  mDatabase.child("users").child(mUserId).child("profile").push().child("uname").setValue(newuser);
+                                FirebaseUser user = task.getResult().getUser();
+                                String mUserId = user.getUid();
 
-
-                                        Intent intent = new Intent(SignUpActivity.this, MainActivity.class);
-                                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                                        startActivity(intent);
-
-                                    } else {
-                                        AlertDialog.Builder builder = new AlertDialog.Builder(SignUpActivity.this);
-                                        builder.setMessage(task.getException().getMessage())
-                                                .setTitle(R.string.login_error_title)
-                                                .setPositiveButton(android.R.string.ok, null);
-                                        AlertDialog dialog = builder.create();
-                                        dialog.show();
-                                    }
+                                DatabaseReference mProfile = mDatabase.child("users").child(mUserId).child("profile");
+                                String[] userDataValues = {fname, lname, uphone};
+                                for(int i = 0; i< userDataValues.length;i++) {
+                                    mProfile.child(UserData[i]).setValue(userDataValues[i]);
                                 }
-                            });
 
+                                Intent intent = new Intent(SignUpActivity.this, MainActivity.class);
+                                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                startActivity(intent);
 
-                }
-
-
-
-
-            }
-        });
+                            } else {
+                                AlertDialog.Builder builder = new AlertDialog.Builder(SignUpActivity.this);
+                                builder.setMessage(task.getException().getMessage())
+                                        .setTitle(R.string.login_error_title)
+                                        .setPositiveButton(android.R.string.ok, null);
+                                AlertDialog dialog = builder.create();
+                                dialog.show();
+                            }
+                        });
+            });
     }
-
 }
--- app/src/main/java/com/murrays/aiderv1/User.java
+++ app/src/main/java/com/murrays/aiderv1/User.java
@@ -3,33 +3,42 @@ package com.murrays.aiderv1;
 public class User {
     public String FirstName;
     public String LastName;
+    public String Phone;
 
-    public User() {
+    public User(String firstName, String lastName, String phone) {
+        this.FirstName = firstName;
+        this.LastName = lastName;
+        this.Phone = phone;
     }
+}
 
-    public String getFirstName() {
+    /*public String getFirstName() {
         return FirstName;
     }
-
     public void setFirstName(String name) {
         FirstName = name;
     }
+
     public String getLastName() {
         return LastName;
     }
-
     public void setLastName(String name) {
         LastName = name;
     }
 
-
-
-
-    public User(String fname, String lname) {
-        FirstName = fname;
-        LastName = lname;
-
+    public String getPhone() {
+        return this.phone;
+    }
+    public void setPhone(String name) {
+        this.phone = name;
     }
 
-}
+
+    public User(String fname, String lname, String phone) {
+        FirstName = fname;
+        LastName = lname;
+        this.phone = phone;
+    }
+
+}*/
 
