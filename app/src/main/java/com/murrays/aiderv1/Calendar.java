d7aef828bbbedec8da88041853a104a1d4c38f29
ssteph-sudo <77129616+ssteph-sudo@users.noreply.github.com>
2022-06-07 18:36:30 -0700

    Wrote code to pass the selected calendar day to next intent, added list view of upcoming

--- app/src/main/java/com/murrays/aiderv1/Calendar.java
+++ app/src/main/java/com/murrays/aiderv1/Calendar.java
@@ -1,24 +1,47 @@
 package com.murrays.aiderv1;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.CalendarView;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Date;
+
 public class Calendar extends Activity{
     CalendarView calendar;
     TextView date_view;
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseUser mFirebaseUser;
+    private DatabaseReference mDatabase;
+    private String mUserId;
+    private String userFname;
+    private String mFamilyID;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mFirebaseUser = mFirebaseAuth.getCurrentUser();
+        mDatabase = FirebaseDatabase.getInstance().getReference();
 
         // By ID we can use each component
         // which id is assign in xml file
@@ -28,7 +51,8 @@ public class Calendar extends Activity{
                 findViewById(R.id.calendar);
         date_view = (TextView)
                 findViewById(R.id.date_view);
-
+        Date currDate = new Date();
+        date_view.setText(currDate.getDate() +"-"+(currDate.getMonth()+1)+"-"+currDate.getYear());
         // Add Listener in calendar
         calendar
                 .setOnDateChangeListener(
@@ -56,11 +80,46 @@ public class Calendar extends Activity{
 
                                 // set this date in TextView for Display
                                 date_view.setText(Date);
+                                String dateToPass = ""+dayOfMonth+(month+1)+year;
+                                gotoNextPage(dateToPass);
+
+
+
                             }
                         });
 
 
+        // Show user Latest Calendar items
+        final ListView listView = (ListView) findViewById(R.id.calendar_recent_calview);
+        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, android.R.id.text1);
+        listView.setAdapter(adapter);
 
+        //   Log.i("Familyid: ", mFamilyID);
+
+        mDatabase.child("family").child("familyID").child("55").child("CalendarItems").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                for (DataSnapshot child : dataSnapshot.getChildren()) {
+                    String dateKey = child.getKey();
+                    String addme = "Date: "+dateKey + "  Event: "+ child.child("Description").getValue(String.class);
+                    adapter.add(addme);
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+
+
+    }
+    private void gotoNextPage(String date){
+        Intent intent = new Intent(Calendar.this,CalendarActivity.class);
+        intent.putExtra("date", date);
+        startActivity(intent);
     }
 
 }
--- app/src/main/java/com/murrays/aiderv1/CalendarActivity.java
+++ app/src/main/java/com/murrays/aiderv1/CalendarActivity.java
@@ -2,13 +2,19 @@ package com.murrays.aiderv1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class CalendarActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_calendar);
+        setContentView(R.layout.activity_calendar_day);
+        TextView tv = (TextView) findViewById(R.id.date_data);
+        Intent intent = getIntent();
+        String data = intent.getStringExtra("date");
+        tv.setText(data);
     }
 }
