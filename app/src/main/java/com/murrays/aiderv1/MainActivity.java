35521bb1580c05ec2213213b98f1d8ba5897ccbc
chitalu7 <chitalu.am@bellevuecollege.edu>
2022-06-20 20:25:07 -0700

    Committing changes

--- app/src/main/java/com/murrays/aiderv1/MainActivity.java
+++ app/src/main/java/com/murrays/aiderv1/MainActivity.java
@@ -1,6 +1,9 @@
 package com.murrays.aiderv1;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -12,19 +15,24 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 import androidx.core.content.ContextCompat;
 
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.murrays.aiderv1.Location.LocationService;
 
 //Sensors
 import android.graphics.Color;
@@ -33,7 +41,20 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.hardware.SensorEvent;
 
-//Location
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+
+//Sensors
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.hardware.SensorEvent;
+
+//NEW LOCATION IMPORTS!
+import android.Manifest;
 import android.Manifest;
 import android.app.ActivityManager;
 import android.content.Context;
@@ -50,7 +71,10 @@ import java.util.Locale;
 
 
 
+
+
 public class MainActivity extends AppCompatActivity {
+
     private static final int REQUEST_CODE_LOCATION_PERMISSION = 1;
     private FirebaseAuth mFirebaseAuth;
     private String userFname;
@@ -58,8 +82,11 @@ public class MainActivity extends AppCompatActivity {
     private Sensor mAccelerometer;
     private double accelerationCurrentValue;
     private double accelerationPreviousValue;
-   // TextView txtAcceleration, txtCurrent, txtPrev;
+    TextView txtAcceleration;  //, txtCurrent, txtPrev;
     //private String mFamilyID;
+
+    DatabaseReference mDatabase;
+
 
 
     private SensorEventListener sensorEventListener = new SensorEventListener() {
@@ -76,6 +103,21 @@ public class MainActivity extends AppCompatActivity {
 
             //txtCurrent.setText("Current = " + /*(int)*/accelerationCurrentValue);
             //txtPrev.setText(("Prev = " + /*(int)*/accelerationPreviousValue));
+
+            //txtAcceleration.setText("Acceleration change = " + /*(int)*/changeInAcceleration);
+
+            if(changeInAcceleration > 12) {
+                //txtAcceleration.setBackgroundColor(Color.RED);
+                sendNotificationToDB();
+
+            }
+            else if (changeInAcceleration > 5) {
+                //txtAcceleration.setBackgroundColor(Color.BLUE);
+            }
+            else if(changeInAcceleration > 2) {
+                //txtAcceleration.setBackgroundColor(Color.GREEN);
+            }
+
            /* txtAcceleration.setText("Acceleration change = " + *//*(int)*//*changeInAcceleration);
 
             if(changeInAcceleration > 12) {
@@ -103,12 +145,14 @@ public class MainActivity extends AppCompatActivity {
         // Initialize Firebase Auth and Database Reference
         mFirebaseAuth = FirebaseAuth.getInstance();
         FirebaseUser mFirebaseUser = mFirebaseAuth.getCurrentUser();
-        DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
+        mDatabase = FirebaseDatabase.getInstance().getReference();
         //txtAcceleration = findViewById(R.id.txtAcceleration);
         //txtCurrent = findViewById(R.id.txtCurrent);
         //txtPrev = findViewById(R.id.txtPrev);
         mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
         mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        startLocationService();
 //        startLocationService();
        /* findViewById(R.id.buttonStartLocationUpdates).setOnClickListener(new View.OnClickListener() {
             //start
@@ -187,8 +231,8 @@ public class MainActivity extends AppCompatActivity {
             });*/
 
             final ListView listView2 = (ListView) findViewById(R.id.notifications_recent);
-            final ArrayAdapter<String> adapter2 = new ArrayAdapter<>(this, R.layout.listview_layout, R.id.text1);
-            listView2.setAdapter(adapter2);
+            final ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.listview_layout, R.id.text1);
+            listView2.setAdapter(adapter);
 
             mDatabase.child("family").child("familyID").child("55").child("Notifications").addValueEventListener(new ValueEventListener() {
                 @Override
@@ -198,7 +242,7 @@ public class MainActivity extends AppCompatActivity {
                         // Get only to 5 records from notifications node
                         if(num <6) {
                             String addme = child.child("NotificationText").getValue(String.class);
-                            adapter2.add(addme);
+                            adapter.add(addme);
                             num++;
                         }
                     }
@@ -210,42 +254,42 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-//    @Override
-//    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-//        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-//        if(requestCode == REQUEST_CODE_LOCATION_PERMISSION && grantResults.length > 0) {
-//            if(grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-//                startLocationService();
-//            } else {
-//                Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show();
-//            }
-//        }
-//    }
-//
-//    private boolean isLocationServiceRunning() {
-//        ActivityManager activityManager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
-//        if(activityManager != null) {
-//            for(ActivityManager.RunningServiceInfo service :
-//                    activityManager.getRunningServices(Integer.MAX_VALUE)) {
-//                if(com.murrays.aiderv1.Location.LocationService.class.getName().equals(service.service.getClassName())) {
-//                    if(service.foreground) {
-//                        return true;
-//                    }
-//                }
-//            }
-//            return false;
-//        }
-//        return false;
-//    }
-//
-//    private void startLocationService() {
-//        if(!isLocationServiceRunning()) {
-//            Intent intent = new Intent(getApplication(), com.murrays.aiderv1.Location.LocationService.class);
-//            intent.setAction(com.murrays.aiderv1.Location.Constants.ACTION_START_LOCATION_SERVICE);
-//            startService(intent);
-//            Toast.makeText(this, "Location service started", Toast.LENGTH_SHORT).show();
-//        }
-//    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == REQUEST_CODE_LOCATION_PERMISSION && grantResults.length > 0) {
+            if(grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                startLocationService();
+            } else {
+                Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private boolean isLocationServiceRunning() {
+        ActivityManager activityManager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        if(activityManager != null) {
+            for(ActivityManager.RunningServiceInfo service :
+                    activityManager.getRunningServices(Integer.MAX_VALUE)) {
+                if(com.murrays.aiderv1.Location.LocationService.class.getName().equals(service.service.getClassName())) {
+                    if(service.foreground) {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+        return false;
+    }
+
+    private void startLocationService() {
+        if(!isLocationServiceRunning()) {
+            Intent intent = new Intent(getApplication(), com.murrays.aiderv1.Location.LocationService.class);
+            intent.setAction(com.murrays.aiderv1.Location.Constants.ACTION_START_LOCATION_SERVICE);
+            startService(intent);
+            Toast.makeText(this, "Location service ", Toast.LENGTH_SHORT).show();
+        }
+    }
 //    private void stopLocationService() {
 //        if(isLocationServiceRunning()) {
 //            Intent intent = new Intent(getApplicationContext(), com.murrays.aiderv1.Location.LocationService.class);
@@ -297,5 +341,79 @@ public class MainActivity extends AppCompatActivity {
         super.onPause();
         mSensorManager.unregisterListener(sensorEventListener);
     }
+
+    public void sendNotificationToDB() {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("ddMMyyyyHHmm");
+
+        java.util.Calendar calendar = java.util.Calendar.getInstance();
+
+        String logData = "";
+        String notificationText = "A family member may have fallen. Please check in to see if they are okay";
+        String calendaritemDate = simpleDateFormat.format(calendar.getTime());
+
+        logData = LocationService.getlatitudeAndLongitude();
+
+        String notID = mDatabase.child("family").child("familyID").child("55").child("Notifications").push().getKey();
+        mDatabase.child("family").child("familyID").child("55").child("Notifications").child(notID).child("Time").setValue(calendaritemDate);
+        mDatabase.child("family").child("familyID").child("55").child("Notifications").child(notID).child("NotificationText").setValue(notificationText);
+        mDatabase.child("family").child("familyID").child("55").child("Notifications").child(notID).child("LogData").setValue(logData);
+
+        listenForDBChanges();
+
+    }
+
+    private void listenForDBChanges(){
+        mDatabase.child("family").child("familyID").child("55").child("Notifications").addChildEventListener(new ChildEventListener()
+                //mDatabase.child("family").child("familyID").child("55").child("Notifications").
+        {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                pushNotification(previousChildName);
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                //pushNotification();
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+    }
+
+    private void pushNotification(String notID){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            NotificationChannel channel =
+                    new NotificationChannel("n", "n", NotificationManager.IMPORTANCE_LOW);
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel);
+        }
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "n")
+                .setContentText("Fuego Aider")
+                .setSmallIcon(R.drawable.icon)
+                .setAutoCancel(false)
+                .setContentText("ALERT!!! One of your family members may have fallen!");
+
+        NotificationManagerCompat managerCompat = NotificationManagerCompat.from(this);
+        managerCompat.notify(999, builder.build());
+    }
+
+
+
+
 
 }
